9deb0cdd6ef6e57ecc15827900d328346747c560
Christopher D. Long <galizur@gmail.com>
2014-02-19 00:03:33 -0500

    MiLB JSON output.

--- baseball/milb_linescore.py
+++ baseball/milb_linescore.py
@@ -9,4 +9,12 @@ response = urllib2.urlopen(linescore_url)
 json_linescore = response.read()
 
 linescore = json.loads(json_linescore)
-pprint.pprint(linescore)
+
+# To see the structure
+#pprint.pprint(linescore)
+
+print 'Game date/time:',linescore['data']['game']['time_date']
+print 'Home team:',linescore['data']['game']['home_team_name']
+print 'Home runs:',linescore['data']['game']['home_team_runs']
+print 'Away team:',linescore['data']['game']['away_team_name']
+print 'Away runs:',linescore['data']['game']['away_team_runs']
